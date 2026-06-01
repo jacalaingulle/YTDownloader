989d56b7efebfceb24ddf5a9c8d23a47977bc215
Jac Alain Gulle <jacalaingulle1@gmail.com>
2026-06-01 13:54:32 +0800

    add frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  url = signal('');
+  format = signal('mp3');
+
+  download(){
+    
+  }
 }
